ccb172c53d32499d5b02e78213c1636daa3d2f96
drbasl <170276829+drbasl@users.noreply.github.com>
2025-11-23 08:26:14 -0500

    Initial commit: مشروع اختبار GitHub

---
+++ hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+برنامج بسيط للترحيب
+"""
+
+def main():
+    print("مرحباً من مشروع GitHub التجريبي!")
+    print("هذا المشروع تم إنشاؤه باستخدام Manus")
+
+if __name__ == "__main__":
+    main()
